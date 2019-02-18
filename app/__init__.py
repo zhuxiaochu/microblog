7ea6d70d20cb1e8c10de4ebb3467c9d1eb21dba0
zhuxiaochu <zhuchu.pei@qq.com>
2019-02-18 16:00:52 +0800

    not imporrtant

--- app/__init__.py
+++ app/__init__.py
@@ -18,56 +18,65 @@ from flask_limiter.util import get_remote_address
 from flask_apscheduler import APScheduler
 
 
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)
-migrate = Migrate(app,db)
-
-login = LoginManager(app)
-login.setup_app(app)
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
 login.login_view = 'login'           #visit 'login_required' when not logged in,redirect to '/login'
 login.login_message = '先登录，好吗？'
-
-moment = Moment(app)
-ckeditor = CKEditor(app)
-csrf = CSRFProtect(app)
-
-limiter = Limiter(app, key_func=get_remote_address)
-
-if not app.debug:
-    #send a email for errors
-    if app.config['MAIL_SERVER']:
-        auth = None
-        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-        secure = None
-        if app.config['MAIL_USE_TLS']:
-            secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Microblog errors',
-            credentials=auth, secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
-
-#save logs in local files
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=102400,
-                                       backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('Miroblog startup~~~~~~~')
-
-mail = Mail(app)
-
+moment = Moment()
+ckeditor = CKEditor()
+csrf = CSRFProtect()
+limiter = Limiter(key_func=get_remote_address)
+mail = Mail()
 scheduler = APScheduler()
-scheduler.init_app(app)
-scheduler.start()
+
+def create_app(config_class=Config):
+
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+    moment.init_app(app)
+    ckeditor.init_app(app)
+    csrf.init_app(app)
+    limiter.init_app(app)
+
+    if not app.debug:
+        #send a email for errors
+        if app.config['MAIL_SERVER']:
+            auth = None
+            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+                auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+            secure = None
+            if app.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+                toaddrs=app.config['ADMINS'], subject='Microblog errors',
+                credentials=auth, secure=secure)
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
+
+    #save logs in local files
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=102400,
+                                           backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.info('Miroblog startup~~~~~~~')
+
+    mail.init_app(app)
+
+    scheduler.init_app(app)
+    scheduler.start()
+
+    return app
+
+app = create_app()
 
 from app import routes,models
